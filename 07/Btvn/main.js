21d9081a197e2a8f3b70b2b2f620702b48e1843d
Dzung Hoang Anh <89538065+1000AoH@users.noreply.github.com>
2022-04-05 00:56:01 +0700

    Add files via upload

---
+++ 07/Btvn/main.js
@@ -0,0 +1,45 @@
+let colors = ["#3498db", "#9b59b6", "#e74c3c", "#2c3e50", "#d35400"];
+let wrap = document.querySelector(".wrap");
+let moreBoxes = document.getElementById("btn");
+let totalBoxes = document.getElementById("score");
+let divBoxes = document.getElementsByClassName("box");
+
+// Khởi tạo khi refresh page
+
+createFiveColors();
+totalBoxes.innerText = `Total box: ${divBoxes.length}`;
+
+// Hàm tạo 5 màu theo đúng thứ tự
+
+function createFiveColors() {
+  for (let i = 0; i < colors.length; i++) {
+    let x = document.createElement("div");
+    x.className = "box";
+    x.setAttribute("onclick", "funciont(){this.remove()}");
+    x.style.backgroundColor = colors[i];
+    wrap.appendChild(x);
+  }
+  addRemove();
+}
+
+// Hàm add thêm event click remove cho từng element trong class.
+
+function addRemove() {
+  for (let i = 0; i < divBoxes.length; i++) {
+    divBoxes[i].addEventListener("click", function () {
+      this.remove();
+    });
+  }
+}
+
+// Event khi click vào thêm màu
+
+moreBoxes.addEventListener("click", function () {
+  createFiveColors();
+});
+
+// Update totalBox đang hiển thị
+
+document.addEventListener("click", function () {
+  totalBoxes.innerText = `Total box: ${divBoxes.length}`;
+});
